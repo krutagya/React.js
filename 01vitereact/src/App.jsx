d8df0ed20cded9c5cebc5edb6a7dce4bbaca1a52
krutagya <krutagya.kaneria.cg@gmail.com>
2024-12-04 11:26:07 +0530

    commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,164 @@
-import Chai from "./Chai"
+import "./index.css";
+import logo from "./Assests/OnePlus-Logo.png";
+import kvpc2 from "./Assests/KV_pc2.png";
+import kvpc6 from "./Assests/KV_pc_6000x1280.png";
+import bigcard from "./Assests/big-card-pc.png";
+import bigcard2 from './Assests/bigcard-pc2.png';
+import bigcard3 from "./Assests/bigcard-pc (1).png";
+import sbanner from "./Assests/SecondaryBanner_OnePlusNord4Lite5G_Desktop.png";
+import sbanner2 from "./Assests/SecondaryBanner_OnePlusPad2_Desktop.png";
+import sbanner3 from "./Assests/SecondaryBanner_OnePlusWatch2R_Desktop.png";
+
+import React from "react";
+
 function App() {
   return (
-    <Chai/>
-  )
+    <>
+      <div className="navbar">
+        <div className="logo"><img src={logo} alt="Logo" /></div>
+        <div className="nav-text1">Phone</div>
+        <div className="nav-text2">Audio</div>
+        <div className="nav-text3">Tablet</div>
+        <div className="nav-text4">Wearables</div>
+        <div className="nav-text5">About</div>
+        <div className="nav-text6">Community</div>
+        <div className="nav-text7">Support</div>
+        <div className="login">
+          <button className="login">login and Purchase</button>
+        </div>
+      </div>
+
+      <div className="hero-sec1" data-aos="fade-right">
+        <div className="hero-img1">
+          <img src={kvpc2} alt="Bass-ed Brilliance" />
+        </div>
+        <div className="hero-img1-text">
+          <h6>Bass-ed Brilliance</h6>
+          <p>OnePlus Nord Buds 3</p>
+        </div>
+        <div className="hero-bottom">
+          <div className="learnmore">
+            <p>Up to 32dB Active Noise Cancellation | 2.0 | Up to 43 hrs Playback</p>
+            <button className="button">Learn More</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="hero-sec2" data-aos="fade-left">
+        <div className="hero-img2"><img src={kvpc6} alt="Absurdly Good" /></div>
+        <div className="hero-img2-text">
+          <h6>Absurdly Good</h6>
+          <h6>Powered by OnePlus Intelligence and AI</h6>
+          <p>OnePlus Nord 4</p>
+        </div>
+        <div className="hero-button1">
+          <div className="learnmore1">
+            <p>Metal Unibody Design | Latest 2024 </p>
+            <p>Snapdragon | 6-Year Fluency</p>
+            <button className="button1">Learn More</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="hero-img3-text" data-aos="slide-right">
+        <div>
+          <h6>Perfection at Play</h6>
+          <p>Oneplus</p>
+          <p>Buds Pro 3</p>
+        </div>
+        <div className="right-img">
+          <img src={bigcard} alt="Buds Pro 3" />
+        </div>
+      </div>
+
+      <div className="hero-button2">
+        <div className="learnmore2">
+          <p>Precision Tuned | Hi-Resolution audio | Adaptive Noise</p>
+          <p>Canceling | Spatial Audio with head tracking</p>
+          <button className="button2">Learn More</button>
+        </div>
+      </div>
+
+      <div className="hero-img4-text" data-aos="slide-right">
+        <div>
+          <h1>It's time to move</h1>
+          <p>OnePlus Watch</p>
+          <p>2R</p>
+        </div>
+        <div className="right-img">
+          <img src={bigcard2} alt="OnePlus Watch 2R" />
+        </div>
+      </div>
+
+      <div className="hero-button3">
+        <div className="learnmore3">
+          <p>Up to 100 Hours Battery Life</p>
+          <p>Enhanced Fitness Tracking</p>
+          <button className="button3">Learn More</button>
+        </div>
+      </div>
+
+      <div className="hero-img5-text" data-aos="slide-right">
+        <div className="right-img">
+          <img src={bigcard3} alt="OnePlus Watch 2R" />
+        </div>
+        <div>
+          <h1>It's time to move</h1>
+          <p>OnePlus Watch</p>
+          <p>2R</p>
+        </div>
+      </div>
+
+      <div className="hero-button4">
+        <div className="learnmore4">
+          <p>Up to 100 Hours Battery Life</p>
+          <p>Enhanced Fitness Tracking</p>
+          <button className="button4">Learn More</button>
+        </div>
+      </div>
+
+      <div className="matrix">
+        <div className="row1">
+          <div className="box1" data-aos="fade-left">
+            <img src={sbanner} alt="OnePlus Nord CE4 Lite 5G" />
+            <div className="uppertext">
+              <p className="txt">Absurdly Entertaining</p>
+              <h1 className="txt">OnePlus Nord CE4 Lite 5G</h1>
+              <p className="txt">5,500 mAh Battery | 80W SUPERVOOC</p>
+              <p className="txt">120 Hz AMOLED Display | Aqua Touch</p>
+            </div>
+          </div>
+
+          <div className="box2" data-aos="fade-right">
+            <img src="https://wallpapers.com/images/featured/oneplus-nqecc8oomap78i1x.webp" alt="OnePlus Nord CE4 Lite 5G" />
+            <p className="txt">Absurdly Entertaining</p>
+            <h1 className="txt">OnePlus Nord CE4 Lite 5G</h1>
+            <p className="txt">5,500 mAh Battery | 80W SUPERVOOC</p>
+            <p className="txt">120 Hz AMOLED Display | Aqua Touch</p>
+          </div>
+        </div>
+
+        <div className="row2">
+          <div className="box3" data-aos="fade-left">
+            <img src={sbanner2} alt="OnePlus Nord CE4 Lite 5G" />
+            <p className="txt">Absurdly Entertaining</p>
+            <h1 className="txt">OnePlus Nord CE4 Lite 5G</h1>
+            <p className="txt">5,500 mAh Battery | 80W SUPERVOOC</p>
+            <p className="txt">120 Hz AMOLED Display | Aqua Touch</p>
+          </div>
+
+          <div className="box4" data-aos="fade-right">
+            <img src={sbanner3} alt="OnePlus Nord CE4 Lite 5G" />
+            <p className="txt">Absurdly Entertaining</p>
+            <h1 className="txt">OnePlus Nord CE4 Lite 5G</h1>
+            <p className="txt">5,500 mAh Battery | 80W SUPERVOOC</p>
+            <p className="txt">120 Hz AMOLED Display | Aqua Touch</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
+
--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-README.md
+### 1. Methods to create a vite folder and there installation
+```bash
+Method 1 :- 
+step1 - npm create vite@latest folder_name
+step2 - have to select react in it 
+step3 - have to select javascript  in it
+step4 - then cd folder name
+step5 - npm i
+step6 - npm run dev
+# This  all folders will create in the folder you  install react
+moz-todo-react
+├── README.md
+├── index.html
+├── node_modules
+├── package-lock.json
+├── package.json
+├── public
+│   └── vite.svg
+├── src
+│   ├── App.css
+│   ├── App.jsx
+│   ├── assets
+│   │   └── react.svg
+│   ├── index.css
+│   └── main.jsx
+└── vite.config.js
+```
+Method 2 :- 
+```bash
+```
+
+
+
+### 2.Describing all the Folders  in the vite folder
+```bash
+1.index.html :- This is the most important top-level file Vite injects your code into this file so that your browser can run it.
+2.node_modules :- It will create an enviroment and the given liberarys that are requires to run the code with in are there in the node_modules . it is simplely important to run the code  
+4.src :- source code have multiply javascript(jsx) file which are install while installing vite and where we'll spend most of our time beacuse all code we have to write within src.
+5.pacakge-lock.json :- it is created through npm and It will store meta data of the project wile in the form of script in which we have no acces to change there commands
+6.package.json :-it is created through vite and It will store meta data of the project wile in the form of script in which we have acces to change there commands in it  
+7.public :- it containes static files that will be serverd directly to your browser without being processed by Vite's build tooling.
+```
+### 3. Exploring first react component <App/>
+```bash
+In a React application, the <App /> component typically serves as the root component. It is the starting point of the React component tree and is usually rendered into the root HTML element of the page.
+![Alt text](https://www.google.com/url?sa=i&url=https%3A%2F%2Fbetterprogramming.pub%2Fcreating-a-simple-app-with-react-js-f6aa88998952&psig=AOvVaw0ORFAM3VX3bZZFkymzo9ZB&ust=1733378073627000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKivydm2jYoDFQAAAAAdAAAAABAE)
+
+```
+
+
+
+### For writing html css and java script (all the syntax)
+```bash
+immport './index.html'  (for importing an CSS)
+import image1 from "./Assest/image1.png";  (for genrating an image from assests folder)
+function Krutgaya()  {
+    return (
+        <>
+
+        </>
+    )
+}
+export default Krutagya
+```bash
+
+
+### The changes we should do in the html
+```bash
+1. <img src={namge1} alt="" />
+2. insted of class we have to write className
+3. 
+```
+
+### The content in the main.jsx file
